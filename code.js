13ae4da4ede448aeb82838e5f5bedb6ecef10b1a
dmoneyballer <drew.gilliebc@gmail.com>
2018-02-06 20:14:19 -0700

    saving addition app

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
 console.log("helloworld");
+
+
+
+
+
+function add(in1, in2) {
+  var in1 = parseInt(document.getElementById("in1").value)
+  var in2 = parseInt(document.getElementById("in2").value)
+
+  console.log("dogs love cats");
+  document.getElementById("answer").value = in1 + in2
+}
+
+// add(in1, in2)
